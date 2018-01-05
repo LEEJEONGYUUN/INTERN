95dfcc61ce038614307f0ca578b2fa863bd5f4c5
unknown <zmfkfk123@gmail.com>
2018-01-05 19:06:30 +0900

    commit

--- ballgame/src/App.js
+++ ballgame/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-       
+      <div className="App">
+        <header className="App-header">
+        
+        </header>
       </div>
     );
   }
